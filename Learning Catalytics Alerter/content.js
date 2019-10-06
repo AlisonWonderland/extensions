f6bde1cdd1497552105c6dab8ab5e7f19864d38d
AlisonWonderland <dimitridawn@gmail.com>
2019-10-06 15:34:40 -0700

    Adding LCA

---
+++ Learning Catalytics Alerter/content.js
@@ -0,0 +1,32 @@
+const body = document.querySelector("body");
+const item = document.getElementById("item");
+
+let questionAdded = false;
+const config = { attributes: true, childList: true, subtree: true };
+
+let myAudio = new Audio(); 
+myAudio.src = chrome.extension.getURL("pinwheel.mp3"); 
+
+function questionAlert(counter) {
+    console.log("playing sound");
+    if(counter < 8) {
+        setTimeout(function() {
+            myAudio.play();
+            ++counter;
+            questionAlert(counter);
+        }, 3000);
+    }
+}
+
+const callback = function(mutationsList, observer) {
+    for(let mutation of mutationsList) {
+        // Inside of the parent will change once the question is added
+        if (mutation.type === 'childList') {
+            questionAlert(0);
+        }
+    }
+};
+
+const observer = new MutationObserver(callback);
+
+observer.observe(item, config);
